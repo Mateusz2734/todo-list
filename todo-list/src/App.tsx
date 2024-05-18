9ea16f7acb2eaafee60a145734d6bb92ce64ec98
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-18 14:35:54 +0200

    chore: Make Breadcrumbs generated dynamically

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,17 +1,34 @@
 import { Routes, Route } from 'react-router-dom';
-import Settings from './pages/SettingsPage';
-import Home from './pages/HomePage';
+
+import NotFound from './pages/NotFound';
+import Settings from './pages/Settings';
+import Home from './pages/Home';
 import Layout from './components/Layout';
 
-export default function JoyOrderDashboardTemplate() {
+export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/settings" element={<Settings />} />
+
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-
   );
 }
 
+export type SitemapItem = {
+  path: string;
+  name: string;
+};
+
+export const sitemap: SitemapItem[] = [
+  { path: "/", name: "Home" },
+  { path: "/settings", name: "Settings" },
+  { path: "/tasks", name: "Tasks" },
+  { path: "/tasks/all", name: "All" },
+  { path: "/tasks/today", name: "Today" },
+  { path: "/tasks/in_progress", name: "In progress" },
+  { path: "/tasks/done", name: "Done" },
+];
--- todo-list/src/components/Breadcrumbs.tsx
+++ todo-list/src/components/Breadcrumbs.tsx
@@ -1,11 +1,16 @@
+import { useLocation } from 'react-router-dom';
+
 import JoyBreadcrumbs from '@mui/joy/Breadcrumbs';
-import Link from '@mui/joy/Link';
 import Typography from '@mui/joy/Typography';
+import Link from '@mui/joy/Link';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
 
+import { sitemap, SitemapItem } from '../App';
 
 export function Breadcrumbs() {
+    const location = useLocation();
+
     return (
         <JoyBreadcrumbs
             aria-label="breadcrumbs" size="sm"
@@ -14,24 +19,64 @@ export function Breadcrumbs() {
         >
             <Link
                 underline="none"
-                color="neutral"
+                color="primary"
                 href="/"
                 aria-label="Home"
             >
                 <HomeRoundedIcon />
             </Link>
-            <Link
-                underline="hover"
-                color="neutral"
-                href="/users"
-                fontSize={12}
-                fontWeight={500}
-            >
-                Users
-            </Link>
-            <Typography color="primary" fontWeight={500} fontSize={12}>
-                My profile
-            </Typography>
+            {getBreadcrumbs(location.pathname, sitemap)}
         </JoyBreadcrumbs>
     );
+}
+
+function getBasePaths(path: string): string[] {
+    const segments = path.split('/').filter(segment => segment);
+    const basePaths: string[] = [];
+
+    segments.reduce((accumulatedPath, currentSegment) => {
+        const newPath = `${accumulatedPath}/${currentSegment}`;
+        basePaths.push(newPath);
+        return newPath;
+    }, '');
+
+    return basePaths;
+}
+
+function getBreadcrumbs(path: string, sitemap: SitemapItem[]): JSX.Element[] {
+    const basePaths = getBasePaths(path);
+    const breadcrumbs: JSX.Element[] = [];
+
+    basePaths.forEach(basePath => {
+        const sitemapItem = sitemap.find(item => item.path === basePath);
+        if (sitemapItem) {
+            breadcrumbs.push(
+                <Link
+                    key={basePath}
+                    underline="hover"
+                    color="primary"
+                    href={basePath}
+                    fontSize={12}
+                    fontWeight={500}
+                >
+                    {sitemapItem.name}
+                </Link>
+            );
+        }
+    });
+
+    if (breadcrumbs.length > 0) {
+        breadcrumbs[breadcrumbs.length - 1] = (
+            <Typography
+                key={basePaths[basePaths.length - 1]}
+                color="neutral"
+                fontWeight={500}
+                fontSize={12}
+            >
+                {breadcrumbs[breadcrumbs.length - 1].props.children}
+            </Typography>
+        );
+    }
+
+    return breadcrumbs;
 }
