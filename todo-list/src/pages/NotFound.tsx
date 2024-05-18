d393c647028f5589d90c8067c78e198631044a94
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-18 14:37:13 +0200

    chore: Add placeholder NotFound page

---
+++ todo-list/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/joy/Typography';
+
+export default function NotFoundPage() {
+    return (
+        <Typography level="h2" component="h1" sx={{ mt: 1, mb: 2 }}>
+            404 Not Found
+        </Typography>
+    );
+}
