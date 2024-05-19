cbe177a0a9828f937e66f56a4ee16149a13fa013
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-19 17:37:46 +0200

    chore: Make DatePicker work properly

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CssVarsProvider } from '@mui/joy/styles';
-import CssBaseline from '@mui/joy/CssBaseline';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '@fontsource/inter';
 
+import JoyCssBaseline from '@mui/joy/CssBaseline';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { extendTheme } from '@mui/joy/styles';
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
+
 import App from './App.tsx';
+
+const materialTheme = materialExtendTheme({}, extendTheme());
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CssVarsProvider disableTransitionOnChange>
-        <CssBaseline />
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </CssVarsProvider>
+      <JoyCssVarsProvider>
+        <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }} >
+          <JoyCssBaseline disableTransitionOnChange />
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </MaterialCssVarsProvider>
+      </JoyCssVarsProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
