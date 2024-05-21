68e056871e562a915be265d5afc9ef97971d216f
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-21 22:00:06 +0200

    chore: Add Layout for task routes

---
+++ src/components/TasksLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
+
+import { sitemap } from "../model/sitemap";
+
+export default function TasksLayout() {
+    const location = useLocation();
+
+    return (
+        <Stack width={"60%"} spacing={2}>
+            <Typography level="h2">
+                {sitemap.find((item) => item.path === location.pathname)?.name}
+            </Typography>
+            <Outlet />
+        </Stack>
+    );
+}
