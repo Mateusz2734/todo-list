db4dc3a196ffff088e68cfffbbb4c8871e701520
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-22 11:52:46 +0200

    chore: Make HoverableIcon more generic

--- src/components/HoverableIcon.tsx
+++ src/components/HoverableIcon.tsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 import { IconButtonProps } from '@mui/joy';
-
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { SvgIconTypeMap } from '@mui/material/SvgIcon';
 import IconButton from '@mui/joy/IconButton';
-import RadioButtonIcon from '@mui/icons-material/RadioButtonUncheckedOutlined';
-import CheckCircleIcon from '@mui/icons-material/CheckCircleOutlined';
 
-interface CustomIconButtonProps extends IconButtonProps { }
+interface CustomIconButtonProps extends IconButtonProps {
+    DefaultIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+    HoverIcon?: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+}
 
-export function HoverableIcon({ ...props }: CustomIconButtonProps) {
+const transition = 'opacity 0.2s ease-in-out, transform 0.2s ease-in-out';
+
+export function HoverableIcon({ DefaultIcon, HoverIcon, ...props }: CustomIconButtonProps) {
     const [hover, setHover] = useState(false);
 
     return (
         <IconButton
             {...props}
-
             sx={{ p: 0, minWidth: 0, minHeight: 0, "&:hover": { backgroundColor: "transparent" } }}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
         >
-            <RadioButtonIcon />
-            <CheckCircleIcon style={{ transition: 'opacity 0.2s ease-in-out, transform 0.2s ease-in-out', opacity: hover ? 1 : 0, position: 'absolute' }} />
+            <DefaultIcon />
+            {HoverIcon && (<HoverIcon style={{ transition: transition, opacity: hover ? 1 : 0, position: 'absolute' }} />)}
         </IconButton>
     );
 };
