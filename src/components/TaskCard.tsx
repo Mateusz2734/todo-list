e3955cec3eb5f16c7328911e5768c67d7f7808f7
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-23 10:50:27 +0200

    feat: Make TaskCard more visually pleasing

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,11 +1,17 @@
-import Radio from '@mui/joy/Radio';
 import Sheet from '@mui/joy/Sheet';
+import Stack from '@mui/joy/Stack';
+import Typography from '@mui/joy/Typography';
+
 import IconButton from '@mui/joy/IconButton';
 import DeleteIcon from '@mui/icons-material/DeleteForeverOutlined';
+import RadioButtonIcon from '@mui/icons-material/RadioButtonUncheckedOutlined';
+import CheckCircleIcon from '@mui/icons-material/CheckCircleOutlined';
+import RestoreIcon from '@mui/icons-material/Restore';
 
-
-import { Task } from '../types';
+import { HoverableIcon } from './HoverableIcon';
 import { priorityColors } from '../model/priority';
+import { Task } from '../types';
+import { DateChip } from './DateChip';
 
 type TaskCardProps = {
     task: Task;
@@ -14,25 +20,41 @@ type TaskCardProps = {
 };
 
 export function TaskCard({ task, onDelete, onMutate }: TaskCardProps) {
+    const hoverableIconProps = task.status === "done" ? {
+        DefaultIcon: RestoreIcon,
+        onClick: () => {
+            const updatedTask: Task = { ...task, status: "todo" };
+            onMutate(task.id, updatedTask);
+        }
+    } : {
+        DefaultIcon: RadioButtonIcon,
+        HoverIcon: CheckCircleIcon,
+        color: priorityColors.find((elem) => elem.priority === task.priority)!.color,
+        onClick: () => {
+            const updatedTask: Task = { ...task, status: "done" };
+            onMutate(task.id, updatedTask);
+        }
+    };
+
     return (
-        <Sheet>
-            <Radio
-                variant="soft"
-                color={priorityColors.find((elem) => elem.priority === task.priority)!.color}
-                size="sm"
-                onChange={() => {
-                    const updatedTask: Task = { ...task, status: "done" };
-                    onMutate(task.id, updatedTask);
-                }}
-            />
-            <h2>{task.name}</h2>
-            <p>{task.description}</p>
-            <p>{task.dueDate}</p>
-            <p>{task.priority}</p>
-            <p>{task.status}</p>
-            <IconButton color="danger" variant="plain" onClick={() => onDelete(task.id)}>
-                <DeleteIcon />
-            </IconButton>
+        <Sheet sx={{ p: 1 }}>
+            <Stack direction="row" spacing={1} width="100%" height="100%">
+                <Stack justifyContent="center">
+                    <HoverableIcon
+                        size="sm"
+                        {...hoverableIconProps}
+                    />
+                </Stack>
+                <Stack width="100%">
+                    <Typography level="title-sm">{task.name}</Typography>
+                    <Typography level="body-sm">{task.description}</Typography>
+                    <DateChip date={task.dueDate} />
+                </Stack>
+
+                <IconButton color="danger" variant="plain" onClick={() => onDelete(task.id)}>
+                    <DeleteIcon />
+                </IconButton>
+            </Stack>
         </Sheet>
     );
 }
